b0a55613182323039b2b33faeb37491c22699360
RealMeKaNiK <dautovic2000@gmail.com>
2022-02-17 23:52:37 +0100

    removing stuff

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import React from "react";
 import Sections from "./components/home/Sections";
-import Services from "./components/services-page/Services";
 import AboutUs from "./components/about-us-page/AboutUs";
 import Contact from "./components/contact-page/Contact";
 import { Routes, Route } from "react-router-dom";
@@ -16,7 +15,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Sections />} />
-          <Route path="/services" element={<Services />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
--- src/components/services-page/Services.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Services = () => {
-  return <div>Bokic okic pokic</div>;
-};
-
-export default Services;
