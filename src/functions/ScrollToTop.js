fcea740b29b792d234fc0177e7e2d12296ede32d
RealMeKaNiK <dautovic2000@gmail.com>
2022-02-22 00:07:35 +0100

    test

--- src/functions/ScrollToTop.js
+++ src/functions/ScrollToTop.js
@@ -5,7 +5,9 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if (pathname !== "/add-ons/marketing") {
+      window.scrollTo(0, 0);
+    }
   }, [pathname]);
 
   return null;
