c90bb9d22cd512d068ad3f225f45079c41cf2422
RealMeKaNiK <dautovic2000@gmail.com>
2022-02-21 23:54:55 +0100

    scroll to top bug fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./functions/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>,
